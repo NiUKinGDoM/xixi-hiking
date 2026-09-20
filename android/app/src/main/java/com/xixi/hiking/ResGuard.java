559a02fb6c1feb84226dc8e84b2c88f020414371
NiUKinGDoM <NiUKinGDoM@users.noreply.github.com>
2026-09-20 17:26:51 +0800

    release: v1.2.2.6 (rebuild: 更新日志清掉开发侧内容 + sw v46)

--- android/app/src/main/java/com/xixi/hiking/ResGuard.java
+++ android/app/src/main/java/com/xixi/hiking/ResGuard.java
@@ -5,10 +5,10 @@ package com.xixi.hiking;
 public final class ResGuard {
     public static final String[][] HASHES = {
         {"index.html", "da97b7a6006e72621a93501f2a123792e758df257857464810f2a63c57648c47"},
-        {"app-core.js", "71830ab5aac539c6245c77759117bafa71f24a8f396a83838586a9b9ade4843b"},
-        {"app-data.js", "49dfaad55b302fcd0bcdfd6b5b76712179d0ec247248dd248beff41aa6902658"},
-        {"app-sync.js", "a60946a89080c9546a7bd9731606d26dc9738da99bdb5f126383daf9e810c0b2"},
-        {"app-init.js", "b665275d840279cc5252c81d0d759771b7e6bff40f408d4819e037b8f58290e2"},
+        {"app-core.js", "62bdbae30d0affa82ae5df5ae01b2444a11eefa50c8a51fe22f515fe3df0e92f"},
+        {"app-data.js", "0474a77d863fdddd503d79944a79b02ba128226222d864d97e18b48238a617ea"},
+        {"app-sync.js", "78f6820eae4b2384177586f2cd506d4637c0820b3c23cb501cbfa0acf739b95a"},
+        {"app-init.js", "405818418440091a7edb51e2f8a2f024bfdcdcfe44b70cde1e4322f323c7e20c"},
         {"assets/support-qr-wechat.jpg", "5c8684a31825a0563d2052a2d83da47f0e6974462a5c38745bc09d4b7e923445"},
         {"assets/support-qr-alipay.jpg", "3b8548df274255a6bd153985d06ccf2c1006d2936a69865ab981d6c1ecf02c77"},
         {"assets/fonts/material-icons.woff2", "8265f64786397d6b832d1ca0aafdf149ad84e72759fffa9f7272e91a0fb015d1"},
